d8bc917b6b57f310184eff1968a772d2ca04c628
mas192 <samjoshva@rediffmail.com>
2021-01-02 19:08:19 -0800

    simple madlib

--- main.py
+++ main.py
@@ -1,64 +1,16 @@
-# import modules
-import os
-import web
-import random
-import urllib.request
-import json
+# simple madlibs
 
-# url = 'http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25'
-#
-#
-# def getRandomMadlibs(url):
-#     madLibUrl = urllib.request.urlopen(url)
-#     print('Madlib', str(madLibUrl.getcode()))
-#     madLib = json.load(madLibUrl)
-#     print(madLib)
+def madlibs():
+    adj = input("Please input an adjective: ")
+    verb1 = input("Please input a past tense verb: ")
+    verb2 = input("Please input a verb: ")
+    famousPerson = input("Please input a famous person name: ")
+    yourName = input("Please input your name: ")
+    crazyVerb = input("Please input a crazy verb: ")
+    crazyNoun = input("Please input a crazy noun: ")
+    funnyAdj1, funnyAdj2, funnyAdj3 = input("Please input three funny adjectives: ").split(sep=",")
 
-# set urls
-
-urls = (
-    '/',
-    'index',
-    '/api',
-    'api',
-    '/api/(.*)',
-    'getMadlibs'
-)
-
-# setup app and render
-
-webApp = web.application(urls, globals())
-render = web.template.render('templates/', base="layout")
-
-
-# / Route
-class Index(object):
-    def GET(self):
-        return render.index(output="", title="")
-
-    def POST(self):
-        form = web.input()
-        output = ""
-        title = form["title"]
-        for i in range(0, len(form) / 2):
-            v = "v" + str(i) if "v" + str(i) in form else ""
-            b = "b" + str(i)
-            if v in form:
-                output += form(v)
-            if b in form:
-                output += form(b)
-        return render.index(output, title=title)
-
-
-# /api route
-class Api(object):
-    def GET(self):
-        return render.api()
-
-
-# /api/random route
-
-
-# Press the green button in the gutter to run the script.
+    madlib = f"There are {adj} differences between {famousPerson} and the rest of the species on earth, but one that has been expressed is that {famousPerson} alone is able to imagine the future. For a long time, {yourName} and perhaps {famousPerson} have {verb1} that future. But it’s now becoming apparent that it’s not all doom and gloom. There’s a chance for {famousPerson} to make {crazyVerb}, to complete our journey of development, manage our impact, and once again become a {crazyNoun} in balance with nature. All we need is the will to do so. We now have the opportunity to create the perfect home for {famousPerson}, and restore the {funnyAdj1}, {funnyAdj2}, and {funnyAdj3} world that {yourName} inherited. Just imagine that."
+    print(madlib)
 if __name__ == '__main__':
-    webApp.run()
+    madlibs()
